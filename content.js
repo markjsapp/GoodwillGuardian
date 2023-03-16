60a6261719d686dd1c971bcfa00f5dc7d591b85c
Mark <markjsapp@gmail.com>
2023-03-16 03:22:08 -0400

    modify css, squashed duplication bugs, added additional fields like time left, alert time, item link, and title

--- content.js
+++ content.js
@@ -1,5 +1,15 @@
+chrome.runtime.sendMessage({ action: "contentScriptReady" });
+
 // Define the XPath
 const timeLeftXPath = '/html/body/app-root/app-layout/main/div/app-detail/div/div[2]/div[2]/div/div[1]/div[1]/span[1]/span';
+const itemNameXPath = '/html/body/app-root/app-layout/main/div/app-detail/div/div[2]/div[2]/div/h1';
+
+// Add a listener for the 'initiateAddAuction' message
+chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+  if (request.action === "initiateAddAuction") {
+    // Handle the addAuction action here
+  }
+});
 
 // Function to extract time left from the XPath
 function getTimeLeft() {
@@ -8,12 +18,24 @@ function getTimeLeft() {
   return node ? node.textContent.trim() : null;
 }
 
+function getTimeLeftAndName() {
+  const timeLeftIterator = document.evaluate(timeLeftXPath, document, null, XPathResult.ANY_TYPE, null);
+  const timeLeftNode = timeLeftIterator.iterateNext();
+  const itemNameIterator = document.evaluate(itemNameXPath, document, null, XPathResult.ANY_TYPE, null);
+  const itemNameNode = itemNameIterator.iterateNext();
+  
+  return {
+    timeLeft: timeLeftNode ? timeLeftNode.textContent.trim() : null,
+    itemName: itemNameNode ? itemNameNode.textContent.trim() : null
+  };
+}
+
 // Listen for a message from the popup script to add an auction
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   if (request.action === 'addAuction') {
-    const timeLeft = getTimeLeft();
-    if (timeLeft) {
-      sendResponse({ success: true, timeLeft });
+    const { timeLeft, itemName } = getTimeLeftAndName();
+    if (timeLeft && itemName) {
+      sendResponse({ success: true, timeLeft, itemName });
     } else {
       sendResponse({ success: false });
     }
--- popup/popup.js
+++ popup/popup.js
@@ -5,67 +5,88 @@ const timeUnit = document.getElementById('timeUnit');
 const alertSound = document.getElementById('alertSound');
 
 // Add auction to list
-function addAuctionToList(timeLeft, url) {
-    const auctionDiv = document.createElement('div');
-    auctionDiv.className = 'auction';
-    auctionDiv.dataset.url = url;
-  
-    const timeLeftSpan = document.createElement('span');
-    timeLeftSpan.textContent = timeLeft;
-    auctionDiv.appendChild(timeLeftSpan);
-  
-    const removeButton = document.createElement('button');
-    removeButton.textContent = 'Remove';
-    removeButton.addEventListener('click', () => {
-      auctionDiv.remove();
-      removeSavedAuction(url);
-    });
-    auctionDiv.appendChild(removeButton);
-  
-    trackedAuctions.appendChild(auctionDiv);
-  
-    // Save the auction and its details
-    saveAuction({ timeLeft, url });
+function addAuctionToList(timeLeft, url, itemName = '') {
+  // Check if the auction with the same URL already exists
+  const existingAuction = document.querySelector(`.auction[data-url="${url}"]`);
+  if (existingAuction) {
+    return;
   }
+
+  const auctionDiv = document.createElement('div');
+  auctionDiv.className = 'auction';
+  auctionDiv.dataset.url = url;
+
+  const itemNameElement = document.createElement('p');
+  itemNameElement.textContent = itemName.slice(0, 35);
+  auctionDiv.appendChild(itemNameElement);
+
+  const timeLeftSpan = document.createElement('span');
+  timeLeftSpan.textContent = `Time left: ${timeLeft}`;
+  auctionDiv.appendChild(timeLeftSpan);
+
+  const alertTimeValue = parseInt(alertTime.value, 10);
+  const alertTimeDisplay = alertTimeValue * (timeUnit.value === 'minutes' ? 1 : 60);
+  const alertTimerSpan = document.createElement('span');
+  alertTimerSpan.textContent = `Alert in: ${alertTimeDisplay} ${timeUnit.value}`;
+  alertTimerSpan.style.color = 'red';
+  auctionDiv.appendChild(alertTimerSpan);
+
+  // Create an anchor element to display the link
+  const auctionLink = document.createElement('a');
+  auctionLink.href = url;
+  auctionLink.textContent = 'View Auction';
+  auctionLink.target = '_blank';
+  auctionLink.rel = 'noopener noreferrer';
+  auctionDiv.appendChild(auctionLink);
+
+  const removeButton = document.createElement('button');
+  removeButton.textContent = 'Remove';
+  removeButton.addEventListener('click', () => {
+    auctionDiv.remove();
+    removeSavedAuction(url);
+  });
+  auctionDiv.appendChild(removeButton);
+
+  trackedAuctions.appendChild(auctionDiv);
+
+  // Save the auction and its details
+  saveAuction({ timeLeft, url, itemName });
+}
 
 // Save auction to storage
 function saveAuction(auction) {
-    chrome.storage.sync.get('auctions', (data) => {
-      const auctions = data.auctions || [];
-      auctions.push(auction);
-      chrome.storage.sync.set({ auctions });
-    });
-  }
+  chrome.storage.sync.get('auctions', (data) => {
+    const auctions = data.auctions || [];
+    auctions.push(auction);
+    chrome.storage.sync.set({ auctions });
+  });
+}
 
 // Remove saved auction
 function removeSavedAuction(url) {
-    chrome.storage.sync.get('auctions', (data) => {
-      const auctions = data.auctions || [];
-      const updatedAuctions = auctions.filter((auction) => auction.url !== url);
-      chrome.storage.sync.set({ updatedAuctions });
-    });
-  }
+  chrome.storage.sync.get('auctions', (data) => {
+    const auctions = data.auctions || [];
+    const updatedAuctions = auctions.filter((auction) => auction.url !== url);
+    chrome.storage.sync.set({ auctions: updatedAuctions }); // Fix the key here
+  });
+}
 
 // Load saved auctions and settings
 function loadSavedData() {
-    chrome.storage.sync.get(['auctions', 'alertTime', 'timeUnit', 'alertSound'], (data) => {
-      if (data.auctions) {
-        data.auctions.forEach((auction) => addAuctionToList(auction.timeLeft, auction.url));
-      }
-  
-      if (data.alertTime) {
-        alertTime.value = data.alertTime;
-      }
-  
-      if (data.timeUnit) {
-        timeUnit.value = data.timeUnit;
-      }
-  
-      if (data.alertSound) {
-        alertSound.value = data.alertSound;
-      }
-    });
-  }
+  chrome.storage.sync.get(['auctions', 'alertTime', 'timeUnit', 'alertSound'], (data) => {
+    if (data.alertTime) {
+      alertTime.value = data.alertTime;
+    }
+
+    if (data.timeUnit) {
+      timeUnit.value = data.timeUnit;
+    }
+
+    if (data.alertSound) {
+      alertSound.value = data.alertSound;
+    }
+  });
+}
 
 // Save settings
 alertTime.addEventListener('change', () => {
@@ -82,53 +103,77 @@ alertTime.addEventListener('change', () => {
 
 // Function to convert time left string to seconds
 function timeLeftToSeconds(timeLeft) {
-    const timeUnits = {
-      'd': 86400, // days to seconds
-      'h': 3600, // hours to seconds
-      'm': 60, // minutes to seconds
-      's': 1 // seconds
-    };
-  
-    const timePattern = /(\d+)([dhms])/g;
-    let match;
-    let seconds = 0;
-  
-    while ((match = timePattern.exec(timeLeft)) !== null) {
-      const value = parseInt(match[1], 10);
-      const unit = match[2];
-      seconds += value * timeUnits[unit];
-    }
-  
-    return seconds;
+  const timeUnits = {
+    'd': 86400, // days to seconds
+    'h': 3600, // hours to seconds
+    'm': 60, // minutes to seconds
+    's': 1 // seconds
+  };
+
+  const timePattern = /(\d+)([dhms])/g;
+  let match;
+  let seconds = 0;
+
+  while ((match = timePattern.exec(timeLeft)) !== null) {
+    const value = parseInt(match[1], 10);
+    const unit = match[2];
+    seconds += value * timeUnits[unit];
   }
 
-document.getElementById('addAuction').addEventListener('click', () => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-      const tab = tabs[0];
-      chrome.tabs.sendMessage(tab.id, { action: 'addAuction' }, (response) => {
-        if (response.success) {
-          addAuctionToList(response.timeLeft, tab.url);
-  
-          const timeLeftSeconds = timeLeftToSeconds(response.timeLeft);
-          const alertInSeconds = parseInt(alertTime.value, 10) * (timeUnit.value === 'minutes' ? 60 : 1);
-          const alarmName = `auction_${Date.now()}`;
-  
-          chrome.alarms.create(alarmName, { delayInMinutes: (timeLeftSeconds - alertInSeconds) / 60 });
-  
-          // Save the alarm's details and the auction's URL to be used when the alarm fires
-          chrome.storage.sync.get('alarms', (data) => {
-            const alarms = data.alarms || [];
-            alarms.push({ name: alarmName, url: tab.url });
-            chrome.storage.sync.set({ alarms });
-          });
-        } else {
-            // Show an error message
-            const errorMessage = document.getElementById('errorMessage');
-            errorMessage.textContent = 'Failed to add auction. Please make sure you are on a valid auction page.';
-          }
-      });
+  return seconds;
+}
+
+function loadSavedAuctions() {
+  chrome.storage.sync.get('auctions', (data) => {
+    const auctions = data.auctions || [];
+    auctions.forEach((auction) => {
+      addAuctionToList(auction.timeLeft, auction.url, auction.itemName);
     });
   });
+}
+
+document.getElementById('addAuction').addEventListener('click', () => {
+  // Get the alertTime input element
+  const alertTime = document.getElementById('alertTime');
+
+  // Check if the alert time is set
+  if (alertTime.value === '' || isNaN(alertTime.value)) {
+    alert('Please set a valid alert time.');
+    return;
+  }
+
+  console.log('addAuction clicked');
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    const tab = tabs[0];
+    chrome.tabs.sendMessage(tab.id, { action: 'addAuction' }, (response) => {
+      if (chrome.runtime.lastError) {
+        console.error(chrome.runtime.lastError.message);
+        return;
+      }
+
+      if (response && response.success) {
+        addAuctionToList(response.timeLeft, tab.url, response.itemName);
+
+        const timeLeftSeconds = timeLeftToSeconds(response.timeLeft);
+        const alertInSeconds = parseInt(alertTime.value, 10) * (timeUnit.value === 'minutes' ? 60 : 1);
+        const alarmName = `auction_${Date.now()}`;
+
+        chrome.alarms.create(alarmName, { delayInMinutes: (timeLeftSeconds - alertInSeconds) / 60 });
+
+        // Save the alarm's details and the auction's URL to be used when the alarm fires
+        chrome.storage.sync.get('alarms', (data) => {
+          const alarms = data.alarms || [];
+          alarms.push({ name: alarmName, url: tab.url });
+          chrome.storage.sync.set({ alarms });
+        });
+      } else {
+        // Show an error message
+        const errorMessage = document.getElementById('errorMessage');
+        errorMessage.textContent = 'Failed to add auction. Please make sure you are on a valid auction page.';
+      }
+    });
+  });
+});
 
 // Set up a listener for alarms
 chrome.alarms.onAlarm.addListener((alarm) => {
@@ -161,3 +206,4 @@ chrome.alarms.onAlarm.addListener((alarm) => {
 
 // Initialize the popup
 loadSavedData();
+loadSavedAuctions();
